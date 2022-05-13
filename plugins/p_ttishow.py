e3c1331b376b16c2ca74c357d365bc65b3491bb2
Vishal Mehta <103929700+EightFlix@users.noreply.github.com>
2022-05-13 09:00:56 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -71,7 +71,7 @@ async def leave_a_chat(bot, message):
         reply_markup=InlineKeyboardMarkup(buttons)
         await bot.send_message(
             chat_id=chat,
-            text='<b>Hello Friends, \nMy admin has told me to leave from group so i go! If you wanna add me again contact my support group.</b>',
+            text='<b>Hello , \nMy admin has told me to leave from group so i go! If you wanna add me again contact my Admin.</b>',
             reply_markup=reply_markup,
         )
 
@@ -110,7 +110,7 @@ async def disable_chat(bot, message):
         reply_markup=InlineKeyboardMarkup(buttons)
         await bot.send_message(
             chat_id=chat_, 
-            text=f'<b>Hello Friends, \nMy admin has told me to leave from group so i go! If you wanna add me again contact my support group.</b> \nReason : <code>{reason}</code>',
+            text=f'<b>Hello , \nMy admin has told me to leave from group so i go! If you wanna add me again contact my Admin.</b> \nReason : <code>{reason}</code>',
             reply_markup=reply_markup)
         await bot.leave_chat(chat_)
     except Exception as e:

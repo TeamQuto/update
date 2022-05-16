5c97f4693d324e1c7d5b7693180d6d9a2cbfc137
Vishal Mehta <103929700+EightFlix@users.noreply.github.com>
2022-05-16 21:53:28 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,6 +1,6 @@
 class script(object):
     START_TXT = """Hello {},
-My Name Is <a href=https://t.me/{}>{}</a>,  Just Add Me To Your Group And Enjoy.Join:-@FastFinder """
+My Name Is <a href=https://t.me/{}>{}</a>,  Just Add Me To Your Group And Enjoy... @FastFinder """
     HELP_TXT = """Hey {}
 Here Is The Help For My Commands."""
     ABOUT_TXT = """👸 Name: {}
